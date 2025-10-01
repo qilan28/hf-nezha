d0a867b85eb32bca54a2587a1f7499f3085d2ef7
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-02 04:39:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import os
 import threading
 import time
 import yaml
-ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', '')        # Argo固定隧道域名,留空即使用临时隧道
-ARGO_AUTH = os.environ.get('ARGO_AUTH', '')            # Argo固定隧道密钥,留空即使用临时隧道
+import datetime
+GH_USER	= os.environ.get('GH_USER', '')# github 的用户名，用于面板管理授权
+GH_BACKUP_USER	= os.environ.get('GH_BACKUP_USER', '')	#在 github 上备份哪吒服务端数据库的 github 用户名
+GH_REPO	= os.environ.get('GH_REPO', '')#在 github 上备份哪吒服务端数据库文件的 github 库
+GH_EMAIL = os.environ.get('GH_EMAIL', '') #github 的邮箱，用于备份的 git 推送到远程库
+GH_PAT = os.environ.get('GH_PAT', '')#github 的 PAT ghp开头的
+ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', '') # Argo固定隧道域名,留空即使用临时隧道
+ARGO_AUTH = os.environ.get('ARGO_AUTH', '')  # Argo固定隧道密钥,留空即使用临时隧道
+DASHBOARD_VERSION = os.environ.get('DASHBOARD_VERSION', 'v1.13.2')#	指定面板的版本，以 v0.00.00 的格式，后续将固定在该版本不会升级，不填则使用默认的 v1.13.2
+NZV1_VERSION = os.environ.get('NZV1_VERSION', 'v1.13.1')#  哪吒V1的版本默认v1.13.1
 
 agent_config = {
     'client_secret': 'MLcD6YnifhoY08B9n129UP5cg2139NYa',
-    'debug': True,
-    'disable_auto_update': False,
+    'debug': False,
+    'disable_auto_update': True,
     'disable_command_execute': False,
     'disable_force_update': False,
     'disable_nat': False,
@@ -19,10 +27,10 @@ agent_config = {
     'ip_report_period': 1800,
     'report_delay': 3,
     'self_update_period': 0,
-    'server': '{ARGO_DOMAIN}',
+    'server': f'{ARGO_DOMAIN}:443',
     'skip_connection_count': False,
     'skip_procs_count': False,
-    'temperature': False,
+    'temperature': True,
     'tls': True,
     'use_gitee_to_upgrade': False,
     'use_ipv6_country_code': False,
@@ -34,7 +42,7 @@ dashboard_config = {
     'avg_ping_count': 2,
     'cover': 1,
     'https': {},  # 空字典
-    'install_host': '{ARGO_DOMAIN}:443',
+    'install_host': f'{ARGO_DOMAIN}:443',
     'ip_change_notification_group_id': 0,
     'jwt_secret_key': '', 
     'jwt_timeout': 1,
@@ -58,6 +66,26 @@ mime_types_content = """types {
     application/xml                       xml;
     application/octet-stream              bin;
     }"""
+
+
+def github(type):
+    if not os.path.exists(f'/data/{GH_REPO}'):
+        os.chdir(f'/data/{GH_REPO}')
+    if type == 1:
+        # 拉取仓库
+        os.system(f"git clone https://{GH_PAT}:x-oauth-basic@github.com/{GH_BACKUP_USER}/{GH_REPO}.git")
+            if not os.path.exists(f'/data/{GH_REPO}'):
+                os.chdir(f'/data/{GH_REPO}')
+                os.system(f'git config --global user.email "{GH_EMAIL}"')
+                os.system(f'git config --global user.name "{GH_BACKUP_USER}"') 
+    if type == 2:
+        #备份上传仓库
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        os.system('git add .')
+        os.system(f'git commit -m "{current_time}"')
+        os.system('git push -u origin main')
+        
+
 def nginx():
     
     # 确保目录存在
@@ -74,27 +102,29 @@ def nginx():
     os.system("/data/nginx1.24/sbin/nginx -c /data/nginx.conf")
 def dv1():
     os.system("rm -r /data/dashboard-linux-amd64.zip /data/dashboard-linux-amd64")
-    if not os.path.exists('/data/data'):
-        os.makedirs('/data/data')
-        with open('/data/data/config.yaml', 'w') as file:
+    if not os.path.exists('/data/dv1'):
+        os.makedirs('/data/dv1')
+    if not os.path.exists('/data/dv1/data'):
+        os.makedirs('/data/dv1/data')
+        with open('/data/dv1/data/config.yaml', 'w') as file:
             yaml.dump(dashboard_config, file, default_flow_style=False)
-        print("配置文件已写入 /data/data/config.yaml")
+        print("配置文件已写入 /data/dv1/data/config.yaml")
         
         # os.system("wget -O '/data/data/config.yaml' -q 'https://raw.githubusercontent.com/qilan28/hf-nezha/refs/heads/main/config.yaml'")
-        os.system("wget -O '/data/data/sqlite.db' -q 'https://github.com/qilan28/hf-nezha/raw/refs/heads/main/sqlite.db'")
+        os.system("wget -O '/data/dv1/data/sqlite.db' -q 'https://github.com/qilan28/hf-nezha/raw/refs/heads/main/sqlite.db'")
     
-    os.system("wget -O '/data/dashboard-linux-amd64.zip' -q 'https://github.com/nezhahq/nezha/releases/download/v1.13.2/dashboard-linux-amd64.zip'")
-    os.system("unzip /data/dashboard-linux-amd64.zip")
-    os.system("rm -r /data/dashboard-linux-amd64.zip")
-    os.system("chmod +x  /data/dashboard-linux-amd64")
-    os.system("mv /data/dashboard-linux-amd64 /data/dv1")
+    os.system(f"wget -O '/data/dashboard-linux-amd64.zip' -q 'https://github.com/nezhahq/nezha/releases/download/{DASHBOARD_VERSION}/dashboard-linux-amd64.zip'")
+    os.system("unzip /data/dv1/dashboard-linux-amd64.zip")
+    os.system("rm -r /data/dv1/dashboard-linux-amd64.zip")
+    os.system("chmod +x  /data/dv1/dashboard-linux-amd64")
+    os.system("mv /data/dv1/dashboard-linux-amd64 /data/dv1")
     threading.Thread(target=nginx, daemon=True).start()
     threading.Thread(target=nv1_agent, daemon=True).start()
-    os.system('/data/dv1 jwt_timeout 48')
+    os.system('/data/dv1/dv1 jwt_timeout 48')
 def nv1_agent():
     time.sleep(30)
     os.system("rm -r /data/nezha-agent_linux_amd64.zip /data/nezha-agent")
-    os.system("wget -O '/data/nezha-agent_linux_amd64.zip' -q 'https://github.com/nezhahq/agent/releases/download/v1.13.1/nezha-agent_linux_amd64.zip'")
+    os.system(f"wget -O '/data/nezha-agent_linux_amd64.zip' -q 'https://github.com/nezhahq/agent/releases/download/{NZV1_VERSION}/nezha-agent_linux_amd64.zip'")
     os.system("unzip /data/nezha-agent_linux_amd64.zip")
     os.system("chmod +x  /data/nezha-agent")
     os.makedirs('/data', exist_ok=True)
@@ -112,6 +142,6 @@ def cloudflared():
     os.system("wget -O '/data/cloudflared-linux-amd64' -q 'https://github.com/cloudflare/cloudflared/releases/download/2025.9.0/cloudflared-linux-amd64'")
     os.system("chmod +x  /data/cloudflared-linux-amd64")
     os.system(f'/data/cloudflared-linux-amd64 tunnel run --protocol http2 --token {ARGO_AUTH}')
-
+github(1)
 threading.Thread(target=cloudflared, daemon=True).start()
 dv1()
