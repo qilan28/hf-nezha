129d966c59f220e03f29f7533ad7f418f12a7c3c
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-02 01:36:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ import os
 import threading
 import time
 def nginx():
+    mime_types_content = """types {
+    text/html                             html htm shtml;
+    text/css                              css;
+    text/javascript                       js;
+    image/gif                             gif;
+    image/jpeg                            jpeg jpg;
+    image/png                             png;
+    text/plain                            txt;
+    application/json                      json;
+    application/xml                       xml;
+    application/octet-stream              bin;
+    }"""
+    # 确保目录存在
+    os.makedirs('/data/nginx1.24', exist_ok=True)
+    # 写入文件
+    with open('/data/nginx1.24/mime.types', 'w') as f:
+        f.write(mime_types_content)
+    # 设置文件权限（可选）
+    os.chmod('/data/nginx1.24/mime.types', 0o644)
+    print("mime.types 文件已创建")
     time.sleep(10)
     os.system("rm -r /data/nginx.conf")
     os.system("wget -O '/data/nginx.conf' -q 'https://raw.githubusercontent.com/qilan28/hf-nezha/refs/heads/main/nginx.conf'")
@@ -12,7 +32,7 @@ def nezha():
     os.system("wget -O '/data/dashboard-linux-amd64.zip' -q 'https://github.com/nezhahq/nezha/releases/download/v1.13.2/dashboard-linux-amd64.zip'")
     os.system("unzip /data/dashboard-linux-amd64.zip")
     os.system("chmod +x  /data/dashboard-linux-amd64")
-    # threading.Thread(target=nginx, daemon=True).start()
+    threading.Thread(target=nginx, daemon=True).start()
     os.system('/data/dashboard-linux-amd64 jwt_timeout 48')
 def cloudflared():
     os.system("rm -r /data/cloudflared-linux-amd64")
