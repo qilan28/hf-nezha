4bd8b20242abe66482dea44ca4e01857e2177737
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-02 23:38:55 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
 source /opt/venv/bin/activate
-# nohup python /data/app.py > /data/app.log 2>&1 &
+nohup python /data/app.py > /data/app.log 2>&1 &
 
 jupyter lab \
     --ip=0.0.0.0 \
