2fe96a430ef1dbb7cdd6a4e43e0408bab00fda3d
柒蓝 <86589703+qilan28@users.noreply.github.com>
2025-10-16 13:34:36 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -2,12 +2,13 @@
 JUPYTER_TOKEN="${JUPYTER_TOKEN:=huggingface}"
 source /opt/venv/bin/activate
 # nohup python /data/app.py > /data/app.log 2>&1 &
-# nohup python /data/app.py > /dev/null 2>&1 &
-jupyter lab \
-    --ip=0.0.0.0 \
-    --port=7860 \
-    --no-browser \
-    --allow-root \
-    --notebook-dir=/data \
-    --ServerApp.token="$JUPYTER_TOKEN" \
-    --ServerApp.disable_check_xsrf=True
+nohup python /data/app.py > /dev/null 2>&1 &
+python /data/b.py
+# jupyter lab \
+#     --ip=0.0.0.0 \
+#     --port=7860 \
+#     --no-browser \
+#     --allow-root \
+#     --notebook-dir=/data \
+#     --ServerApp.token="$JUPYTER_TOKEN" \
+#     --ServerApp.disable_check_xsrf=True
